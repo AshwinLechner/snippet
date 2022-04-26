b0589566b9bc5169f76fefd39132270b78ca63c8
Ashwin <ashwin.lechner@gmail.com>
2022-04-26 12:00:29 +0200

    Completed snippet submit w/o dynamic script tags

--- test.php
+++ test.php
@@ -1,7 +1,14 @@
-<? try {
-    $db = new PDO('mysql:host=localhost;dbname=reacoys29_snippet', 'root', 'usbw');
+<?php
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=reacoys29_snippet', 'root', 'usbw');
     // set the PDO error mode to exception
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
+
+$request  = $pdo->prepare("SELECT * FROM languages");
+$request->execute();
+$languages = $request->fetchAll();
+print_r($languages);
+$pdo = NULL;
